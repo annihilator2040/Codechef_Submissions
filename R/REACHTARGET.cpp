66d1b89125b5eef9245c91531da16781d626e31a
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-02 14:12:17 +0530

    Create REACHTARGET.cpp

---
+++ R/REACHTARGET.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/REACHTARGET
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i ,T,X,Y;
+	cin>>T;
+	for (i=0 ; i<T; i++)
+	{
+	    cin>>X>>Y;
+	    int h = X-Y;
+	    cout<<h<<endl;
+	}
+	return 0;
+}
+ 
