27026ad87a1030fee8da267b52a364ec03b1b003
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-02 20:20:23 +0530

    Create AGELIMIT.cpp

---
+++ A/AGELIMIT.cpp
@@ -0,0 +1,23 @@
+https://www.codechef.com/problems/AGELIMIT
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i, T, X, Y, A;
+	cin >> T;
+	for(i = 0; i < T; i++)
+	{
+	    cin>>X>>Y>>A;
+	    if ((A>=X)&&(Y>A))
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
