10d2e93eaeadd2efa7e05c9c9e7d3266ca77e8a4
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-02 16:23:44 +0530

    Create BIRYANI.cpp

---
+++ B/BIRYANI.cpp
@@ -0,0 +1,17 @@
+https://www.codechef.com/problems/BIRYANI
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i, T, X, Y;
+	cin >> T;
+	for(i = 0; i < T; i++)
+	{
+        cin>>X>>Y;
+        int h;
+        h = X*Y;
+        cout<<h<<endl;
+	}
+	return 0;
+}
