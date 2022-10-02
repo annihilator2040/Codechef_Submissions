4ad0631d5c50fdd32f311f7d95b48d1a3184ab12
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-02 16:26:55 +0530

    Create FIT.cpp

---
+++ F/FIT.cpp
@@ -0,0 +1,16 @@
+https://www.codechef.com/problems/FIT
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i, T, X, Y, A, h;
+	cin >> T;
+	for(i = 0; i < T; i++)
+	{
+	  cin>>X;
+	  h = (X*2)*5;
+	  cout<<h<<endl;
+	}
+	return 0;
+}
