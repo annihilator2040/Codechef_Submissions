1668f62c753ed72c9f7749653d693122cff656ed
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-02 17:26:35 +0530

    Create BURGERS.cpp

---
+++ B/BURGERS.cpp
@@ -0,0 +1,23 @@
+https://www.codechef.com/problems/BURGERS
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i, T, A, B;
+	cin >> T;
+	for(i = 0; i < T; i++)
+	{
+	    cin>>A>>B;
+	    if(A>=B)
+	    {
+	        cout<<B<<endl;
+	    }
+	    else if (A<=B)
+	    {
+	        cout<<A<<endl;
+	    }
+    
+	}
+	return 0;
+}
