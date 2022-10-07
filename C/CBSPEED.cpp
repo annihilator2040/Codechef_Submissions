589a753d74184ed5ac7fabb15a0d34068995b90b
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-07 11:26:07 +0530

    Create CBSPEED.cpp

---
+++ C/CBSPEED.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/CBSPEED
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int X, Y, h;
+	cin >> X>>Y;
+	if (X<Y)
+	{
+	    cout<<"YES";
+	}
+	else
+	{
+	    cout<<"NO";
+	}
+	return 0;
+}
