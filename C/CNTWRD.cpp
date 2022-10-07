8ed87c67d61477f31e8fd3b3a821e108e39b8c81
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-07 11:36:30 +0530

    Create CNTWRD.cpp

---
+++ C/CNTWRD.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/problems/CNTWRD
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, M, h, i, T;
+	cin>>T;
+	for (i=0; i<T; i++)
+	{
+	    cin>>N>>M;
+	    h = N*M;
+	    cout <<h<<endl;
+	}
+	return 0;
+}
