9201fdae9e665428658529abf130322854c2df05
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-11 12:02:53 +0530

    Create WATERREQ.cpp

---
+++ W/WATERREQ.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/WATERREQ
+
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+    int N, t, i ;
+    cin>>t;
+    for (i=0; i<t; i++)
+    {
+        cin>>N;
+        int h = N*2;
+        cout<<h<<endl;
+    }
+    return 0;
+}
