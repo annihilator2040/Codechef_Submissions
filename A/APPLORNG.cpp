4c46be68b9ff9365043c8ea0b37e99a2ce77b87b
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-11 18:33:44 +0530

    Create APPLORNG.cpp

---
+++ A/APPLORNG.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/submit/APPLORNG
+
+#include <iostream>
+using namespace std;
+ int main(){
+     int x, a, b;
+     cin>>x;
+     cin>>a>>b;
+     int h = a+b;
+     if (h<=x)
+     cout<<"Yes"<<endl;
+     else 
+     cout <<"No"<<endl;
+    return 0 ;
+ }
