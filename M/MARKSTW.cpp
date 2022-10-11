2ddedec9197e7d844d5aaa96b5e893c3a37dcd6a
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-11 19:13:56 +0530

    Create MARKSTW.cpp

---
+++ M/MARKSTW.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/MARKSTW
+
+#include <iostream>
+using namespace std ;
+
+int main(){
+    float x, y;
+    cin>>x>>y;
+    if (x>=(2*y))
+    cout<<"Yes\n";
+    else 
+    cout<<"No\n";
+    return 0;
+}
