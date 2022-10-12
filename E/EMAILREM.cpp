71579051fb09073d525d68a327971df622d40203
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-12 20:43:15 +0530

    Create EMAILREM.cpp

---
+++ E/EMAILREM.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/EMAILREM
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int N, U ;
+	cin >>N>>U;
+	int h = N-U;
+	cout<<h<<endl;
+	return 0;
+}
