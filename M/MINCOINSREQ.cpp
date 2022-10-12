ad032bd250a7a26956ccf39a6b0526b236332089
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-10-12 21:09:21 +0530

    Create MINCOINSREQ.cpp

---
+++ M/MINCOINSREQ.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/MINCOINSREQ
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int i, T, X ;
+	cin >> T ;
+	for(i = 0; i < T; i++)
+	{
+        cin>>X ;
+        int h = X%10;
+        if (h==0)
+        cout<<h<<endl;
+        else 
+        cout<<h<<endl;
+	}
+	return 0;
+}
