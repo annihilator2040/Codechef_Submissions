de250d00eeb83f1e478a9e83565c7e04803564df
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-11-24 23:44:13 +0530

    Create SQUATS.cpp

---
+++ S/SQUATS.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/problems/SQUATS
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, t, i;
+    cin>>t;
+    for(i=0; i<t; i++)
+    {
+        cin>>x;
+        int h = x*15;
+        cout<<h<<endl;
+    }
+    return 0;
+}
