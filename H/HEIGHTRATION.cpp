569fb7e64b74488d935e3b7ea9b47d863df670eb
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2022-11-24 23:48:37 +0530

    Create HEIGHTRATION.cpp

---
+++ H/HEIGHTRATION.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/problems/HEIGHTRATION
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+    if (a>b)
+    {
+        cout<<a<<endl;   
+    }
+    else 
+    {
+        cout<<b<<endl;
+    }
+    return 0;
+}
