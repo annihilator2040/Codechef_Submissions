fefe6979d730b8ae0308ebfd5700e73ea09ae9fc
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2023-02-16 19:53:03 +0530

    Create KITCHENSPICE.cpp

---
+++ K/KITCHENSPICE.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/KITCHENSPICE
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y, t, i, h ;
+    cin>>t;
+    for(i=0 ; i<t; i++)
+    {
+        cin>>x>>y;
+        h = y-x ;
+        cout<<h<<endl ;
+        
+    }
+    return 0 ;
+}
