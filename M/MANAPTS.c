ff557f16c4e66ac093ca4e34df26830468b31cd0
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2023-02-20 01:14:21 +0530

    Create MANAPTS.c

---
+++ M/MANAPTS.c
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/MANAPTS
+
+#include <stdio.h>
+int main(){
+    int t, i, x, y;
+    scanf("%d",&t);
+    for(i=0;i<t;i++){
+        scanf("%d%d",&x,&y);
+        int h = 
+        printf("%d\n",y/x);
+    }
+    return 0;
+}
