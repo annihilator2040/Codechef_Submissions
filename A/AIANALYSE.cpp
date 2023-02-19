09654d5042fed1e345117729fce358cafb0ef7ce
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2023-02-20 00:44:04 +0530

    Create AIANALYSE.cpp

---
+++ A/AIANALYSE.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/problems/AIANALYSE
+
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+    int c;
+    cin>>c;
+    if (c<=1000)
+    cout<<"Yes\n";
+    else
+    cout<<"No\n";
+    return 0 ;
+}
