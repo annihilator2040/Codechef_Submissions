9c1f2d11f8abbd802dd550e36d60cbe6ebe664b1
kuldeep2040 <83647746+kuldeep2040@users.noreply.github.com>
2023-03-03 13:18:33 +0530

    Create ODDSUMPAIR.c

---
+++ O/ODDSUMPAIR.c
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/ODDSUMPAIR
+
+#include <stdio.h>
+int main(){
+    int t, a, b, c ;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++){
+        scanf("%d%d%d",&a,&b,&c);
+        if((a+b)%2!=0 || (a+c)%2!=0 || (b+c)%2!=0){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
