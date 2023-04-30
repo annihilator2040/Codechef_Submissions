cea5066acdc6434c5bd6ed0e5cdd8742c710daf9
Kuldeep Singh <83647746+kuldeep2040@users.noreply.github.com>
2023-04-30 11:08:01 +0530

    Create BIRDFARM.c

---
+++ B/BIRDFARM.c
@@ -0,0 +1,22 @@
+// https://www.codechef.com/problems/BIRDFARM
+
+#include <stdio.h>
+int main(){
+    int t, x, y, z;
+    scanf("%d",&t);
+    for(int i=0; i<t; i++){
+        scanf("%d%d%d",&x,&y,&z);
+        if(z%x==0 && z%y==0){
+            printf("ANY\n");
+        }
+        else if(z%y==0){
+            printf("DUCK\n");
+        }
+        else if(z%x==0){
+            printf("CHICKEN\n");
+        }
+        else if(z%x!=0 && z%y!=0){
+            printf("NONE\n");
+        }
+    }
+}
