85ee40ee1b3c17ea1cea7601f8ec05107d9ef616
Kuldeep Singh <83647746+kuldeep2040@users.noreply.github.com>
2023-05-30 18:02:40 +0530

    Create FSQRT.c

---
+++ F/FSQRT.c
@@ -0,0 +1,19 @@
+// https://www.codechef.com/problems/FSQRT
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) 
+{
+	int t,n,r;
+	scanf("%d",&t);
+	while(t--)
+	{
+	scanf("%d",&n);  
+	r=sqrt(n);
+	printf("%d\n",r);      
+	}
+	return 0;
+}
+
+
